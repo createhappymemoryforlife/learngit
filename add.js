f557b5696c0bb926f9675b658fec29da3300b89f
fangzhou <fangzhou@chelun.com>
2020-10-27 09:55:38 +0800

    checkout

--- add.js
+++ add.js
@@ -3,4 +3,14 @@ console.log('dev');
 console.log('merge');
 console.log('devnomerge');
 console.log('devnomerge2');
-console.log('devnomerge3');
+
+// git checkout -- <filename>
+// 把指定文件从暂存区复制到工作区,用来丢弃工作区对该文件的修改
+
+// git checkout HEAD~ -- <filename>
+// 如果指定从某个commit恢复指定文件,这会同时改变暂存区与工作区
+
+// git checkout -p
+// git checkout -p 表示进入交互模式,只恢复部分变化
+
+//
